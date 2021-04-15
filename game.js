a080ea8b62734baafa05d96369c576709d5adb2b
ahmed <52543589+amdMUST@users.noreply.github.com>
2021-04-15 12:19:55 -0400

    basic conway completed
    still need
    - pattern button
    - login button
    - everything else

--- game.js
+++ game.js
@@ -1,3 +1,18 @@
+
+// constant values 
+const size = 50;
+let currBoard = [size], nextBoard = [size];
+let gameStarted = false;
+let timer, generationSpeed = 1000;
+let generation = 1;
+
+// function that sets up the board
+function setupGame(){
+    createBoard();
+    createBoardArrays();
+    changeGeneration(generation++);
+}
+
 // we need to create the table onload
 function createBoard(){
 
@@ -7,8 +22,7 @@ function createBoard(){
 
     table.setAttribute('id', 'gameGrid');
 
-    // now make each row and column based on a variable
-    var size = 50;
+    // now make each row and column based on the variable size
     for( let i = 0; i < size; i++ ){
 
         var row = document.createElement('tr');
@@ -36,15 +50,215 @@ function clickedCell(){
 
     // first get the coordinate of the cell, so that we can locate it
     var splitCell = this.id.split('_');
-    var row = splitCell[1];
-    var col = splitCell[2];
+    var i = splitCell[1];
+    var j = splitCell[2];
 
     // toggle it on or off depending on previous state
     if( this.className == 'inactive' ){
         this.setAttribute('class', 'active');
+        currBoard[i][j] = 1;
     }
     else{
         this.setAttribute('class', 'inactive');
+        currBoard[i][j] = 0;
     }
 
+}
+
+// function to create the arrays to store the values of the cells
+function createBoardArrays(){
+
+    // need to make everything a 2d array first
+    for( var i = 0; i < size; i++ ){
+        currBoard[i] = new Array(size);
+        nextBoard[i] = new Array(size);
+    }
+
+    // then fill everything with 0's to show there are no filled arrays
+    resetArray();
+}
+
+// sets all the values in the board to zero
+function resetArray(){
+    
+    for( var i = 0; i < size; i++ ){
+        for( var j = 0; j < size; j++ ){
+            currBoard[i][j] = 0;
+            nextBoard[i][j] = 0;
+        }
+    }
+}
+
+// resets all the values
+function reset(){
+
+    // end the timer
+    endLoop();
+    
+    // make all the squares inactive
+    for( var i = 0; i < size; i++ ){
+        for( var j = 0; j < size; j++ ){
+            var cell = document.getElementById('cell_'+i+'_'+j);
+            cell.setAttribute('class', 'inactive');
+        }
+    }
+
+    generation = 1;
+    changeGeneration(generation);
+
+    // reset all the values in the arrays
+    resetArray();
+}
+
+// this will incrememnt by one generation
+function increment(){
+
+    // check the neighbors of each piece on the board
+    for( var i = 0; i < size; i++ ){
+        for( var j = 0; j < size; j++ ){
+            
+            var n_count = getNeighbors(i, j);
+            
+            if( currBoard[i][j] == 1 ){
+                // if active
+                
+                if( n_count < 2 || n_count > 3 ){
+                    nextBoard[i][j] = 0;
+                }
+                else if( n_count == 3 || n_count == 2 ){
+                    nextBoard[i][j] = 1;
+                }
+
+            }
+            else if( currBoard[i][j] == 0 ){
+                // if inactive 
+
+                if( n_count == 3 )
+                    nextBoard[i][j] = 1;
+            }
+        }
+    }
+
+    // update the generation
+    for( var i = 0; i < size; i++ ){
+        for( var j = 0; j < size; j++ ){
+            currBoard[i][j] = nextBoard[i][j];
+            nextBoard[i][j] = 0;
+        }
+    }
+
+    // update the cells to show the new generation
+    changeGeneration(generation++);
+    updateBoard();
+    
+}
+
+// this function returns how many neighbors a cell has
+function getNeighbors( i, j ){
+    var count = 0;
+
+    // check top neighbor
+    if( i - 1 >= 0 && currBoard[i-1][j] == 1 ){
+        count++;
+    }
+
+    // check top right neighbor
+    if( (i - 1 >= 0 && j + 1 < size) && currBoard[i-1][j+1] == 1 ){
+        count++;
+    }
+
+    // check top left neighbor
+    if( (i - 1 >= 0 && j - 1 >= 0) && currBoard[i-1][j-1] == 1 ){
+        count++;
+    }
+
+    // check right neighbor
+    if( j + 1 < size && currBoard[i][j+1] == 1 ){
+        count++;
+    }
+
+    // check left neighbor
+    if( j - 1 >= 0 && currBoard[i][j-1] == 1 ){
+        count++;
+    }
+
+    // check bottom neighbor
+    if( i + 1 < size && currBoard[i+1][j] == 1 ){
+        count++;
+    }
+
+    // check bottom right neighbor
+    if( (i + 1 < size && j + 1 < size) && currBoard[i+1][j+1] == 1 ){
+        count++;
+    }
+    
+    // check bottom left neighbor
+    if( (i + 1 < size && j - 1 >= 0) && currBoard[i+1][j-1] == 1 ){
+        count++;
+    }
+
+    return count;
+}
+
+// updates the board according to current board.
+function updateBoard(){
+
+    for( var i = 0; i < size; i++ ){
+        for( var j = 0; j < size; j++ ){
+
+            var cell = document.getElementById('cell_'+i+'_'+j);
+            
+            if( currBoard[i][j] == 1 ){
+                cell.setAttribute('class', 'active');
+            }
+            else{ 
+                cell.setAttribute('class', 'inactive');
+            }
+        }
+    }
+}
+
+// function that increments 23 times
+function twenty_three_increments(p){
+
+    started = true;
+    setTimeout( function(){
+        p++;
+        increment();
+        if( p < 23 )
+            twenty_three_increments(p)
+    }, generationSpeed);
+
+}
+
+// function that loops every 1 sec to update to new generation
+function loop(){
+
+    increment();
+    if( started ){
+        timer = setTimeout( loop, generationSpeed );
+    }
+
+}
+
+// function to start gerneration loop
+function startLoop(){
+
+    started = true;
+    loop();
+}
+
+// function to end gerneration loop
+function endLoop(){
+
+    if( started ){
+        started = false;
+        clearTimeout(timer);
+    }
+}
+
+// change the generation text
+function changeGeneration( current ){
+    var text = document.getElementById('generation');
+    text.innerHTML = 'Generation ' + current;
 }
